c795af60b26892389002a2debc9c4894120aade6
Abdullah Ghazal <abdullah.m.gh98@gmail.com>
2024-02-21 10:17:55 +0200

    Built the critic score component

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,23 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+function CriticScore({ score }: Props) {
+  let color = score > 85 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <>
+      <Badge
+        colorScheme={color}
+        fontSize="16px"
+        paddingX="10px"
+        borderRadius="7px"
+      >
+        {score}
+      </Badge>
+    </>
+  );
+}
+
+export default CriticScore;
