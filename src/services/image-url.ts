d2f2b9f553ca4025ff197d5ba99aaa65851c7c85
Abdullah Ghazal <abdullah.m.gh98@gmail.com>
2024-02-27 16:52:35 +0200

    Implementing games sorting functionality

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,8 @@
 function getCroppedImgUrl(url: string) {
-    const target = "media/";
-    const index = url.indexOf(target) + target.length; 
-    return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+  if (!url) return null; // when sorting, some games does not have an image so the programm broke.
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 }
 
 export default getCroppedImgUrl;
