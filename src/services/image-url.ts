e31a1562817745ed4d53063380b4d8d046e610ba
Abdullah Ghazal <abdullah.m.gh98@gmail.com>
2024-02-28 09:39:00 +0200

    Fixed the issue of games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,8 @@
+import imagePlaceholder from "../assets/image-placeholder.webp";
+
 function getCroppedImgUrl(url: string) {
-  if (!url) return null; // when sorting, some games does not have an image so the programm broke.
+  if (!url) return imagePlaceholder;
+
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
