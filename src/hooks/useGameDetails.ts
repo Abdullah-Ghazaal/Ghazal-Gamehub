60b1535bf1cfa99e5c4f1f88b0a01c308ed1dcb9
Abdullah Ghazal <abdullah.m.gh98@gmail.com>
2024-04-28 04:44:58 +0300

    Refactoring: delete the gameDetailsService and replace it with gamesService which achieve the same task

--- src/hooks/useGameDetails.ts
+++ src/hooks/useGameDetails.ts
@@ -1,14 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import gameDetailsService from "../services/gameDetailService";
 import ms from "ms";
-import { Game } from "../services/gamesService";
+import gamesService, { Game } from "../services/gamesService";
 
 function useGameDetails(slug: string) {
   return useQuery<Game, Error>({
     ////
     queryKey: ["game details", slug],
     ////
-    queryFn: () => gameDetailsService.get(slug),
+    queryFn: () => gamesService.get(slug),
     ////
     staleTime: ms("24h"),
   });
--- src/services/gameDetailService.ts
+++
@@ -1,6 +0,0 @@
-import APIClient from "./api-client";
-import { Game } from "./gamesService";
-
-const gameDetailsService = new APIClient<Game>("/games");
-
-export default gameDetailsService;
