7323e3e6f24d2942e6b947b2d0e415063bf951f6
Abdullah Ghazal <abdullah.m.gh98@gmail.com>
2024-07-01 04:11:19 +0300

    Hide the gradient background light for mobile

--- src/components/BackgroundGradient.tsx
+++ src/components/BackgroundGradient.tsx
@@ -1,18 +1,20 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Show } from "@chakra-ui/react";
 
 function BackgroundGradient() {
   return (
-    <Box
-      width="400px"
-      height="400px"
-      borderRadius="100%"
-      backgroundColor="teal.600"
-      position={"fixed"}
-      top="35vh"
-      left="-60"
-      zIndex="-10"
-      filter={"blur(160px)"}
-    />
+    <Show breakpoint="(min-width: 769px)">
+      <Box
+        width="320px"
+        height="320px"
+        borderRadius="100%"
+        backgroundColor="teal.600"
+        position={"fixed"}
+        top="35vh"
+        left="-60"
+        zIndex="-10"
+        filter={"blur(160px)"}
+      />
+    </Show>
   );
 }
 
